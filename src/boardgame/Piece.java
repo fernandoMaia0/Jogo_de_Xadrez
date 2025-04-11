bc6cb2ac720ae32f6e5fe5956e6dee183111e8d9
fernandoMaia0 <fernandomaia.dev@gmail.com>
2025-04-11 16:03:04 -0300

    Start to implement  Board and Piece

---
+++ src/boardgame/Piece.java
@@ -0,0 +1,17 @@
+package boardgame;
+
+public class Piece {
+    
+    protected Position position;
+    private Board bord;
+
+    public Piece(Board bord) {
+        this.bord = bord;
+        position = null;
+    }
+
+    protected Board getBord() {
+        return bord;
+    }
+        
+}
